3f3d7990a0a74dc6ad50cd55fddcecab92135379
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2019-03-04 10:54:38 +0900

    add valve-ctl

--- modules/bastion/files/setup.sh
+++ modules/bastion/files/setup.sh
@@ -19,5 +19,6 @@ EOF
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 
 runuser -l ec2-user -c "curl -sL https://raw.githubusercontent.com/opsnow/kops-cui/master/tools.sh | bash"
+runuser -l ec2-user -c "curl -sL https://raw.githubusercontent.com/opsnow-tools/valve-ctl/master/install.sh | bash"
 runuser -l ec2-user -c "[ -d ~/kops-cui ] && pushd ~/kops-cui/ && git pull && popd"
 runuser -l ec2-user -c "[ -d ~/kops-cui ] || git clone https://github.com/opsnow/kops-cui"
