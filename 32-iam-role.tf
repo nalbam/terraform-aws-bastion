bb33493c56bfc8479eaa633cddc92d0eb278a30c
nalbam <me@nalbam.com>
2019-11-20 17:43:39 +0900

    Interpolation-only expressions are deprecated

--- 32-iam-role.tf
+++ 32-iam-role.tf
@@ -1,7 +1,7 @@
 # iam role
 
 resource "aws_iam_role" "this" {
-  name        = "${var.name}"
+  name        = var.name
   description = "iam role for ${var.name}"
 
   assume_role_policy = <<POLICY
@@ -22,7 +22,7 @@ POLICY
 }
 
 resource "aws_iam_instance_profile" "this" {
-  name = "${var.name}"
+  name = var.name
   role = aws_iam_role.this.name
 }
 
